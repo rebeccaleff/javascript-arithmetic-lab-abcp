7848ba56ad242fadee664731ed1713368a8cd1bf
Rebecca Leff <rebeccacleff@gmail.com>
2018-01-20 06:20:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function makeInt (n) {
   return parseInt(n)
 }
 
-function preserveDecimal(n) {
-  n = parseFloat (n, 10)
-  return preserveDecimal (n)
+function preserveDecimal(m) {
+  n = parseFloat (m)
+  return parseFloat (m)
 }
